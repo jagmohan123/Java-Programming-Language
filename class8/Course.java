87c8d062a29ba41f7b840224a616e0936da2132a
Jagmohan Rai <102997544+jagmohan123@users.noreply.github.com>
2025-09-26 22:17:47 +0530

    Package and String, Final in java

---
+++ class8/Course.java
@@ -0,0 +1,10 @@
+
+import learningpackage.MernStack;
+
+public class Course {
+    public static void main(String[] args) {
+        System.out.println("run ho rha hai");
+        MernStack ob = new MernStack();
+        ob.courseName();
+    }
+}
---
+++ class8/learningpackage/MernStack.java
@@ -0,0 +1,9 @@
+package learningpackage1;
+ public class MernStack {
+   public void courseName() {
+        System.out.println("This is the mern stack course");
+    }
+    public static void main(String[] args) {
+        System.out.println("run ho rha hai");
+    }
+}
